4b297297a28e7eb8d37b5835df3d82a816b3cb83
mendelsh <mendelshperber@gmail.com>
2025-03-21 11:09:39 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+
+func main() {
+
+	http.HandleFunc("/", handleSearch)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func handleSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+
