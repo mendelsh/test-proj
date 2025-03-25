120e14b49338ed0e03c87802b6d61229d26fe333
BatelDayan <bateldayan212438@gmail.com>
2025-03-25 15:57:16 +0200

    script-tag-batel.sh

--- script-tag-batel.sh
+++ script-tag-batel.sh
@@ -6,3 +6,19 @@ echo "$latest_tag"
 
 commit=$(git show "$latest_tag" | head -n 1 | awk '{print $2}')
 echo "$commit"
+commits=($(git log --pretty=oneline "$commit" | nl | awk '{print $2}'))
+len=${#commits[@]}
+declare -A changes
+for ((i=0; i<$len-1; i++))
+do
+   files=($(git diff --name-only "${commits[$i]}" "${commits[$i+1]}"))
+   for file in "$files"
+    do
+        changes["$file"]="$file"
+    done    
+done
+
+for file in "${changes[@]}"
+do
+    echo "$file"
+done
