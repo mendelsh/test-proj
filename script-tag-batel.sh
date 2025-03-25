f563fb490f3ef4b973814812ed6513c2f10a23be
BatelDayan <bateldayan212438@gmail.com>
2025-03-25 12:29:09 +0200

    not important

--- script-tag-batel.sh
+++ script-tag-batel.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-rel_array=$(git tag | grep "^v")
-echo "${rel_array[*]}"
- 
+
+#git tag | grep "^Rel"
+latest_tag=$(git tag | grep "^Rel" | tail -n 1) 
+echo "$latest_tag"
+
+commit=$(git show "$latest_tag" | head -n 1 | awk '{print $2}')
+echo "$commit"
