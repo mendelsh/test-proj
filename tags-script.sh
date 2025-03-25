e2f8a74d8d097c5d8a66c7a7b9ba3b7342858da6
BatelDayan <bateldayan212438@gmail.com>
2025-03-25 12:39:36 +0200

    tags-script.sh

--- tags-script.sh
+++ tags-script.sh
@@ -1,26 +1,22 @@
 #!/usr/bin/env bash
-
-
 arr=($(git tag |grep "^Rel"))
-
 arr=$(python3 -c "
-
+import sys
 from functools import cmp_to_key
 
 def compare_elements(a, b):
-    
     a = str(a)[4:].split('.')
     b = str(b)[4:].split('.')
-
-    for index in range(len(a)):
-        if a[index] > b[index]:
+    for index in range(min(len(a), len(b))):
+        if int(a[index]) > int(b[index]):
             return -1
-        elif a[index] < b[index]:
+        elif int(a[index]) < int(b[index]):
             return 1
-    return 0
+    return len(b) - len(a)
 
-sorted_arr = sorted($arr, key=cmp_to_key(compare_elements))
-print(sorted_arr)
+# Use sys.argv to pass the list of tags
+tags = sys.argv[1:]
+sorted_arr = sorted(tags, key=cmp_to_key(compare_elements))
+print(' '.join(sorted_arr))
 ")
-
 echo ${arr[*]}
