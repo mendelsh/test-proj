61706fb704f10d1d88ad20150e492570e568da0f
mendelsh <mendelshperber@gmail.com>
2025-03-25 16:04:40 +0200

    echo

--- script-tag-batel.sh
+++ script-tag-batel.sh
@@ -5,7 +5,10 @@ latest_tag=$(git tag | grep "^Rel" | tail -n 1)
 echo "$latest_tag"
 
 commit=$(git show "$latest_tag" | head -n 1 | awk '{print $2}')
-echo "$commit"
+echo "commit hash: $commit"
+echo
+echo "files chaimges:"
+
 commits=($(git log --pretty=oneline "$commit" | nl | awk '{print $2}'))
 len=${#commits[@]}
 declare -A changes
@@ -20,5 +23,5 @@ done
 
 for file in "${changes[@]}"
 do
-    echo "$file"
+    echo "  $file"
 done
