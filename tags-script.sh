1567e1eec9ccf20c24cdb1987bccd6f6ee42d5b6
mendelsh <mendelshperber@gmail.com>
2025-03-25 12:17:41 +0200

    tags-script.sh

--- tags-script.sh
+++ tags-script.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 
 
-arr=($(git tag --sort=-creatordate |grep "^v"))
+arr=($(git tag |grep "^Rel"))
 
-echo ${arr[0]}
+arr=$(python3 -c "
+
+from functools import cmp_to_key
+
+def compare_elements(a, b):
+    
+    a = str(a)[4:].split('.')
+    b = str(b)[4:].split('.')
+
+    for index in range(len(a)):
+        if a[index] > b[index]:
+            return -1
+        elif a[index] < b[index]:
+            return 1
+    return 0
+
+sorted_arr = sorted($arr, key=cmp_to_key(compare_elements))
+print(sorted_arr)
+")
+
+echo ${arr[*]}
