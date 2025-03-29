4439df06c1aa85779a4bdc5c3d4f8cacb000087f
BatelDayan <bateldayan212438@gmail.com>
2025-03-29 17:48:27 +0300

    created Dockerfile + env

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 
 func main() {
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	http.HandleFunc("/", handleSearch)
 
 	fmt.Println("Server is running on port 8080")
@@ -18,7 +23,14 @@ func main() {
 }
 
 func handleSearch(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("MENDEL")
+	mail := os.Getenv("MAIL")
+	pass := os.Getenv("PASSWORD")
+	url := os.Getenv("URL")
+	
+
 	fmt.Fprintf(w, "Hello, %s! \nbatel added this part", r.URL.Path[1:])
+	fmt.Fprintf(w, "my name is %s ,my email is %s ,my password is %s, my website is %s"   name, mail, pass, url )
 }
 
 
